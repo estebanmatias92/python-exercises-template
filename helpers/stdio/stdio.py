22f7be4487a6266fac6d64e21b6e21d32286f86a
estebanmatias92 <estebanmatias92@gmail.com>
2022-07-11 03:40:32 +0000

    Added float input validation function

--- helpers/stdio/stdio.py
+++ helpers/stdio/stdio.py
@@ -7,24 +7,40 @@
 def int_input(phrase):
     value = input(phrase)
 
-    if not value.isnumeric():
-        print("The value is not numeric")
+    try:
+        return int(value)
+    except ValueError:
+        print("The value is not an integer")
         exit()
-
-    return int(value)
 
 
 #
 # Receive an string as parameter for the input.
 #
-# Exit the program with an error message if the returned value from the input is numeric.
+# Exit the program with an error message if the returned value from the input is numeric or decimal.
 # Returns value if value was not numeric.
 #
 def string_input(phrase):
     value = input(phrase)
 
-    if value.isnumeric():
+    if not value.alpha() or not value.alnum():
         print("The value is not string")
         exit()
 
     return value
+
+
+#
+# Receive an string as parameter for the input.
+#
+# Exit the program with an error message if the returned value from the input is not decimal.
+# Returns value casted to int if value was numeric.
+#
+def float_input(phrase):
+    value = input(phrase)
+
+    try:
+        return float(value)
+    except ValueError:
+        print("The value is not a floating point")
+        exit()
